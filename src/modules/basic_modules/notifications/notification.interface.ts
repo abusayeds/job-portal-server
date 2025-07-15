2d4e4b7e07fea04644e447dc2688e828fd6d6251
abusayeds <abusayedstudent855@gmail.com>
2025-07-15 18:20:32 +0600

    update notification model

--- src/modules/basic_modules/notifications/notification.interface.ts
+++ src/modules/basic_modules/notifications/notification.interface.ts
@@ -1,9 +1,9 @@
 
-import { Document, Types } from "mongoose";
+import { Types } from "mongoose";
 
 // Define the INotification type
 export type INotification = {
   userId: Types.ObjectId;
   jobId?: Types.ObjectId;
   notification: string;
-} & Document;
+} 
